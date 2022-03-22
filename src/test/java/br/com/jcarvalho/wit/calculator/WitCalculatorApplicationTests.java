a7c4c338f7601be420edbd447a363663a832c51e
Jose Josemar de Carvalho <jcarvalhogo@gmail.com>
2022-03-21 23:13:18 -0300

    criação do projeto

---
+++ src/test/java/br/com/jcarvalho/wit/calculator/WitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcarvalho.wit.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
