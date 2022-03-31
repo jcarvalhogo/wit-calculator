dc3d38b49aa57e5dd34db6776bedfe2d291ff87b
Jose Josemar de Carvalho <jcarvalhogo@gmail.com>
2022-03-31 18:07:26 -0300

    criado projeto service calculo

---
+++ wit-service/src/main/java/br/com/jcarvalho/witcalculator/witservice/WitServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcarvalho.witcalculator.witservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitServiceApplication.class, args);
+    }
+
+}
---
+++ wit-service/src/test/java/br/com/jcarvalho/witcalculator/witservice/WitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcarvalho.witcalculator.witservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
